54ead3d06d73aa79a3c2cebc7541924a84406301
praveeninbasekaran <ipraveen88@gmail.com>
2025-03-24 08:54:31 +0530

    Create AlertStageDecisionDto.java

---
+++ src/main/java/org/acme/scheduler/AlertStageDecisionDto.java
@@ -0,0 +1,9 @@
+public class AlertStageDecisionDto {
+    public Long decisionId;
+    public Integer stage;
+    public String role;
+    public String decision;
+    public LocalDateTime actionDate;
+    public String findings;
+    public String riskManagementAction;
+}
