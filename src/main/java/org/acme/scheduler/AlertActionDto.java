04160726f2db5a1c2f72a8c5044e739bea2f00be
praveeninbasekaran <ipraveen88@gmail.com>
2025-03-24 09:02:27 +0530

    Create AlertActionDto.java

---
+++ src/main/java/org/acme/scheduler/AlertActionDto.java
@@ -0,0 +1,12 @@
+public class AlertActionDto {
+    public Long actionId;
+    public Integer actionStage;
+    public String actionTaken;
+    public Integer actionBy;
+    public String actionRole;
+    public LocalDateTime actionDate;
+    public String actionNotes;
+    public String assignNotes;
+    public String findings;
+    public String riskManagementAction;
+}
