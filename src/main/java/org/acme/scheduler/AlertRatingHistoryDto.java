66a0887b097fa2c4547320d83ce6d5ebc578d245
praveeninbasekaran <ipraveen88@gmail.com>
2025-03-24 08:54:01 +0530

    Create AlertRatingHistoryDto.java

---
+++ src/main/java/org/acme/scheduler/AlertRatingHistoryDto.java
@@ -0,0 +1,6 @@
+public class AlertRatingHistoryDto {
+    public Long ratingId;
+    public String ratingType; // Current or Previous
+    public String ratingValue;
+    public LocalDateTime ratingDate;
+}
