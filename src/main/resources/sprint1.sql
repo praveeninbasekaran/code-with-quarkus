1984c024ae478183ea0102d2d90f81dd60581fa9
praveeninbasekaran <ipraveen88@gmail.com>
2025-05-22 12:01:26 +0530

    Update sprint1.sql

--- src/main/resources/sprint1.sql
+++ src/main/resources/sprint1.sql
@@ -11,3 +11,27 @@ CREATE TABLE role_dropdown_mapping (
     legality VARCHAR
 );
 
+-- Role: Risk level 1 framework owner → Only Risk Level 1 is applicable
+INSERT INTO role_dropdown_mapping (role, risk_level1)
+VALUES
+('Risk level 1 framework owner', 'Financial Crime'),
+('Risk level 1 framework owner', 'Compliance'),
+('Risk level 1 framework owner', 'Operations');
+
+-- Role: Risk level 2 framework owner → Risk Level 1 and 2
+INSERT INTO role_dropdown_mapping (role, risk_level1, risk_level2)
+VALUES
+('Risk level 2 framework owner', 'Financial Crime', 'Sanctions'),
+('Risk level 2 framework owner', 'Financial Crime', 'Transaction failure');
+
+-- Role: Business function L1 (ILOD) → Business Function L1, L2, Country
+INSERT INTO role_dropdown_mapping (role, business_function_l1, business_function_l2, country)
+VALUES
+('Business function L1 (ILOD)', 'Investment banking', 'Financial Market', 'Srilanka'),
+('Business function L1 (ILOD)', 'Investment banking', 'Financial Market', 'India');
+
+-- Role: Country Risk owner → Business Function L1, L2, Country
+INSERT INTO role_dropdown_mapping (role, business_function_l1, business_function_l2, country)
+VALUES
+('Country Risk owner', 'Investment banking', 'Financial Market', 'Srilanka'),
+('Country Risk owner', 'Investment banking', 'Financial Market', 'India');
